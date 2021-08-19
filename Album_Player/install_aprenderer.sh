6cc9e05d39a8685684f310dd34c636e1e451326d
sam0402 <sam0402@gmail.com>
2021-08-19 23:46:15 +0800

    Add files via upload

---
+++ Album_Player/install_aprenderer.sh
@@ -0,0 +1,16 @@
+#!/bin/busybox ash
+
+. /etc/init.d/tc-functions
+. /var/www/cgi-bin/pcp-functions
+
+useBusybox
+TARGET=`cat /etc/sysconfig/backup_device`
+cd /mnt/$TARGET
+echo pcp-aplayer_render.tcz >>onboot.lst
+cd optional
+wget https://raw.githubusercontent.com/sam0402/piCorePlayer-Album_Player/master/pcp-aplayer_render.tcz
+
+pcp_write_var_to_config USER_COMMAND_3 "sudo%20taskset%20-c%203%20aplayer;%20sudo%20taskset%20-c%203%20aprenderer"
+echo "Rebooting..."
+sleep 3
+pcp br
