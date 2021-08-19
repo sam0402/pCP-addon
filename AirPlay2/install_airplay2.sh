459c72aa882026a7cac6029dec331a2962935367
sam0402 <sam0402@gmail.com>
2021-08-19 11:55:47 +0800

    Add files via upload

---
+++ AirPlay2/install_airplay2.sh
@@ -0,0 +1,25 @@
+
+#!/bin/busybox ash
+
+. /etc/init.d/tc-functions
+. /var/www/cgi-bin/pcp-functions
+
+useBusybox
+TARGET=`cat /etc/sysconfig/backup_device`
+
+tce-load -i -w pcp-shairportsync.tcz avahi.tcz libavahi.tcz glib2.tcz pcp-libffmpeg.tcz
+
+cd /mnt/$TARGET/optional
+wget https://raw.githubusercontent.com/sam0402/pCP-addon/main/AirPlay2/pcp-shairportsync.tcz
+wget https://raw.githubusercontent.com/sam0402/pCP-addon/main/AirPlay2/pcp-shairportsync.tcz.md5.txt
+wget https://raw.githubusercontent.com/sam0402/pCP-addon/main/AirPlay2/pcp-shairportsync.tcz.dep
+wget https://raw.githubusercontent.com/sam0402/pCP-addon/main/AirPlay2/libconfig_popt.tcz
+wget https://raw.githubusercontent.com/sam0402/pCP-addon/main/AirPlay2/libconfig_popt.tcz.md5.txt
+wget https://raw.githubusercontent.com/sam0402/pCP-addon/main/AirPlay2/libplist.tcz
+wget https://raw.githubusercontent.com/sam0402/pCP-addon/main/AirPlay2/libplist.tcz.md5.txt
+wget https://raw.githubusercontent.com/sam0402/pCP-addon/main/AirPlay2/libsodium.tcz
+wget https://raw.githubusercontent.com/sam0402/pCP-addon/main/AirPlay2/libsodium.tcz.md5.txt
+
+echo "Rebooting..."
+sleep 3
+pcp br
