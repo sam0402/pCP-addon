7d61da6f1ffd8b2e2ea3cb68ca366fed8eb416a2
sam0402 <sam0402@gmail.com>
2021-09-04 23:45:54 +0800

    Create config_usb_dac.sh

---
+++ config/config_usb_dac.sh
@@ -0,0 +1,15 @@
+#!/bin/busybox ash
+
+. /etc/init.d/tc-functions
+. /var/www/cgi-bin/pcp-functions
+
+useBusybox
+TARGET=`cat /etc/sysconfig/backup_device`
+cd /mnt/$TARGET
+wget https://raw.githubusercontent.com/sam0402/pCP-addon/main/config/mydata8_usb.tgz
+mv mydata8_usb.tgz mydata.tgz
+cd optional
+echo 'libasound2.tcz' >pcp-squeezelite.tcz.dep
+echo "Rebooting..."
+sleep 2
+pcp br
