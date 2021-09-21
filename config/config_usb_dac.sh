18c028adb1303c376e081f09b040caefcb5716ec
sam0402 <sam0402@gmail.com>
2021-09-21 16:45:07 +0800

    Update config_usb_dac.sh

--- config/config_usb_dac.sh
+++ config/config_usb_dac.sh
@@ -8,7 +8,7 @@ TARGET=`cat /etc/sysconfig/backup_device`
 cd /mnt/$TARGET
 wget https://raw.githubusercontent.com/sam0402/pCP-addon/main/config/mydata8_usb.tgz
 mv mydata8_usb.tgz mydata.tgz
-ln -s squeezelite-dsd squeezelite
+ln -s /mnt/$TARGET/tce/squeezelite-dsd squeezelite
 cd optional
 echo 'libasound2.tcz' >pcp-squeezelite.tcz.dep
 echo "Rebooting..."
