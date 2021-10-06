cd589946d00e625c08e4f0cfcfedc5f806befebc
sam0402 <sam0402@gmail.com>
2021-10-06 11:04:57 +0800

    Update config_usb_dac.sh

--- config/config_usb_dac.sh
+++ config/config_usb_dac.sh
@@ -9,7 +9,9 @@ cd /mnt/$TARGET
 wget https://raw.githubusercontent.com/sam0402/pCP-addon/main/config/mydata8_usb.tgz
 mv mydata8_usb.tgz mydata.tgz
 ln -s /mnt/$TARGET/tce/squeezelite-dsd squeezelite
+sed -i '/rpi-vc.tcz/d' onboot.lst
 cd optional
+sed -i '/rng-tools.tcz/d' pcp.tcz.dep
 echo 'libasound2.tcz' >pcp-squeezelite.tcz.dep
 echo "Rebooting..."
 sleep 2
