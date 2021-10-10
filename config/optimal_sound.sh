ff0002c8bcf2b7ed4aaa48d4a11f40b54583a3b8
sam0402 <sam0402@gmail.com>
2021-10-10 20:50:06 +0800

    Update optimal_sound.sh

--- config/optimal_sound.sh
+++ config/optimal_sound.sh
@@ -13,7 +13,10 @@ cd optional
 sed -i '/rng-tools.tcz/d' pcp.tcz.dep
 echo 'libasound2.tcz' >pcp-squeezelite.tcz.dep
 
-cat << 'EOL' >> /opt/bootlocal.sh
+NUM=`grep -c 'taskset' /opt/bootlocal.sh`
+if [ $NUM -eq 0 ]
+then
+  cat << 'EOL' >> /opt/bootlocal.sh
 sleep 30
 taskset -p 0x00000008 $(pgrep squeezelite*)
 pkill -f udhcpc
@@ -29,7 +32,7 @@ umount /tmp/tcloop/ca-certificate
 umount /tmp/tcloop/ncurses
 umount /tmp/tcloop/dialog
 EOL
-
+fi
 echo "Rebooting..."
 sleep 2
 pcp br
