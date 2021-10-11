2e039d893bd3801b1633be6bfa7c04a48da1420f
sam0402 <sam0402@gmail.com>
2021-10-12 07:49:06 +0800

    Update optimal_sound.sh

--- config/optimal_sound.sh
+++ config/optimal_sound.sh
@@ -12,7 +12,7 @@ cd optional
 sed -i '/rng-tools.tcz/d;/dialog.tcz/d' pcp.tcz.dep
 echo 'libasound2.tcz' >pcp-squeezelite.tcz.dep
 pcp_write_var_to_config USER_COMMAND_1 "%23sleep+60%3b+uhubctl+-l+1-1+-a+0%3bsleep+120%3bpkill+-f+httpd%3bumount+%2ftmp%2ftcloop%2fpcp-8.0.0-www"
-pcp_write_var_to_config USER_COMMAND_2 "sleep+180%3bif+%5b+%60ps+%7c+grep+-c+%27sshd%3a+tc%27%60+-lt+3+%5d%3bthen+(pkill+-f+ssh%3bumount+%2ftmp%2ftcloop%2fca-certificates+%2ftmp%2ftcloop%2fopenssh)%3bif"
+pcp_write_var_to_config USER_COMMAND_2 "sleep+180%3bif+%5b+%60ps+%7c+grep+-c+%27sshd%3a+tc%27%60+-lt+3+%5d%3bthen+(pkill+-f+ssh%3bumount+%2ftmp%2ftcloop%2fopenssh)%3bif"
 if [ `grep -c 'taskset' /opt/bootlocal.sh` -eq 0 ]
 then
   sed -i '11,$d' /opt/bootlocal.sh
@@ -24,7 +24,7 @@ taskset -p 0x00000008 $(pgrep squeezelite*)
 pkill -f udhcpc
 sleep 60
 umount /tmp/tcloop/uhubctl /tmp/tcloop/libusb /tmp/tcloop/libudev
-umount /tmp/tcloop/ncurses /tmp/tcloop/alsa-utils
+umount /tmp/tcloop/ncurses /tmp/tcloop/alsa-utils /tmp/tcloop/ca-certificates
 #--- Add by Sam0402
 EOL
 fi
