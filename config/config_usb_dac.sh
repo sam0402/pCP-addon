7a4937f523f96b7345dc7c503e2919878a6a4480
sam0402 <sam0402@gmail.com>
2021-10-11 16:37:24 +0800

    Update config_usb_dac.sh

--- config/config_usb_dac.sh
+++ config/config_usb_dac.sh
@@ -8,35 +8,4 @@ TARGET=`cat /etc/sysconfig/backup_device`
 cd /mnt/$TARGET
 wget https://raw.githubusercontent.com/sam0402/pCP-addon/main/config/mydata8_usb.tgz
 mv mydata8_usb.tgz mydata.tgz
-ln -s /mnt/$TARGET/tce/squeezelite-dsd squeezelite
-sed -i '/rpi-vc.tcz/d' onboot.lst
-sed -i '/ntfs-3g.tcz/d' onboot.lst
-cd optional
-sed -i '/rng-tools.tcz/d' pcp.tcz.dep
-sed -i '/dialog.tcz/d' pcp.tcz.dep
-echo 'libasound2.tcz' >pcp-squeezelite.tcz.dep
-
-if [ `grep -c 'taskset' /opt/bootlocal.sh` -eq 0 ]
-then
-  cat << 'EOL' >> /opt/bootlocal.sh
-
-#--- Add by Sam0402
-sleep 20
-taskset -p 0x00000008 $(pgrep squeezelite*)
-pkill -f udhcpc
-sleep 60
-umount /tmp/tcloop/uhubctl
-umount /tmp/tcloop/libusb
-umount /tmp/tcloop/libudev
-umount /tmp/tcloop/ncurses
-umount /tmp/tcloop/alsa-utils
-sleep 90
-[ `ps | grep -c 'sshd: tc'` -lt 3 ] && pkill -f ssh
-umount /tmp/tcloop/ca-certificates
-umount /tmp/tcloop/openssh
-#--- Add by Sam0402
-EOL
-fi
-echo "Rebooting..."
-sleep 2
-pcp br
+wget -O - https://raw.githubusercontent.com/sam0402/pCP-addon/main/config/optimal_sound.sh | sudo sh
