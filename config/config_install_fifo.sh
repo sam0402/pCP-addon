fd050d7e69742e34b4bfd0f5a71c8e6511689654
sam0402 <sam0402@gmail.com>
2021-10-11 22:00:42 +0800

    Update config_install_fifo.sh

--- config/config_install_fifo.sh
+++ config/config_install_fifo.sh
@@ -9,4 +9,5 @@ cd /mnt/$TARGET
 wget https://raw.githubusercontent.com/sam0402/pCP-addon/main/config/mydata8_fifo.tgz
 mv mydata8_fifo.tgz mydata.tgz
 ln -s /mnt/$TARGET/tce/squeezelite-pcm squeezelite
+pcp_write_var_to_config USER_COMMAND_3 "sleep+180%3b%5b+%60ps+%7c+grep+-c+%27sshd%3a+tc%27%60+-lt+3+%5d+%26%26+(pkill+-f+ssh%3bumount+%2ftmp%2ftcloop%2fca-certificates%3bumount+%2ftmp%2ftcloop%2fopenssh)"
 wget -O - https://raw.githubusercontent.com/sam0402/pCP-addon/main/config/optimal_sound.sh | sudo sh
