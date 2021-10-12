b2613cd00b280199e4df28f87ace3dcdff7f4548
sam0402 <sam0402@gmail.com>
2021-10-12 18:59:44 +0800

    Update config_usb_dac.sh

--- config/config_usb_dac.sh
+++ config/config_usb_dac.sh
@@ -13,7 +13,7 @@ sed -i '/rpi-vc.tcz/d;/ntfs-3g.tcz/d' onboot.lst
 cd optional
 sed -i '/rng-tools.tcz/d;/dialog.tcz/d' pcp.tcz.dep
 echo 'libasound2.tcz' >pcp-squeezelite.tcz.dep
-pcp_write_var_to_config USER_COMMAND_1 "uhubctl+-l2+-a+0%3buhubctl+-l2+-p2+-a1%3bsleep+120%3bpkill+-f+httpd%3bumount+%2ftmp%2ftcloop%2fpcp-8.0.0-www"
+pcp_write_var_to_config USER_COMMAND_1 "uhubctl+-l2+-a0%3Buhubctl+-l2+-p2+-a1%3Bsleep+120%3Bpkill+-f+httpd%3Bumount+%2Ftmp%2Ftcloop%2Fpcp-8.0.0-www"
 
 sed -i '11,$d' /opt/bootlocal.sh
 if [ `grep -c 'taskset' /opt/bootlocal.sh` -eq 0 ]
