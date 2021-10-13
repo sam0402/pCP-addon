d79b6ccc26b3c37f40d07b10cc19f998aed58997
sam0402 <sam0402@gmail.com>
2021-10-13 20:58:16 +0800

    Update install_aprenderer_arm64.sh

--- Album_Player/install_aprenderer_arm64.sh
+++ Album_Player/install_aprenderer_arm64.sh
@@ -8,7 +8,8 @@ TARGET=`cat /etc/sysconfig/backup_device`
 cd /mnt/$TARGET
 echo pcp-aplayer_render_arm64.tcz >>onboot.lst
 cd optional
-wget https://raw.githubusercontent.com/sam0402/piCorePlayer-Album_Player/master/pcp-aplayer_render_arm64.tcz
+sudo -u tc tce-load -i ca-certificates.tcz
+wget https://raw.githubusercontent.com/sam0402/pCP-addon/main/Album_Player/pcp-aplayer_render_arm64.tcz
 
 pcp_write_var_to_config USER_COMMAND_3 "sudo%20taskset%20-c%203%20aplayer;%20sudo%20taskset%20-c%203%20aprenderer"
 echo "Rebooting..."
