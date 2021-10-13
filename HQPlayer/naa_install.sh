dbb67653a400da616e1afb81a19bf5d12e9f4ea3
sam0402 <sam0402@gmail.com>
2021-10-13 20:23:37 +0800

    Update naa_install.sh

--- HQPlayer/naa_install.sh
+++ HQPlayer/naa_install.sh
@@ -10,7 +10,7 @@ echo naa.tcz >>onboot.lst
 cd optional
 wget https://raw.githubusercontent.com/sam0402/pCP-addon/main/HQPlayer/naa.tcz
 
-pcp_write_var_to_config USER_COMMAND_3 ""
+pcp_write_var_to_config USER_COMMAND_3 "%2fusr%2fsbin%2fnetworkaudiod+%26"
 echo "Rebooting..."
 sleep 3
 pcp br
