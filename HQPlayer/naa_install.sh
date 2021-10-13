8b0ffb4321bdfb77229281379efd7cf231d7af6e
sam0402 <sam0402@gmail.com>
2021-10-13 20:15:37 +0800

    Update naa_install.sh

--- HQPlayer/naa_install.sh
+++ HQPlayer/naa_install.sh
@@ -8,7 +8,7 @@ TARGET=`cat /etc/sysconfig/backup_device`
 cd /mnt/$TARGET
 echo naa.tcz >>onboot.lst
 cd optional
-wget https://raw.githubusercontent.com/sam0402/pCP-addon/HPPlayer/naa.tcz
+wget https://raw.githubusercontent.com/sam0402/pCP-addon/main/HQPlayer/naa.tcz
 
 pcp_write_var_to_config USER_COMMAND_3 ""
 echo "Rebooting..."
