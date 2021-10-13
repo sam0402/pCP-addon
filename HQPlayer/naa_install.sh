dea4bfb8c0510ecd68e0c3e37329b0b5a842cca2
sam0402 <sam0402@gmail.com>
2021-10-13 20:14:12 +0800

    Create naa_install.sh

---
+++ HQPlayer/naa_install.sh
@@ -0,0 +1,16 @@
+#!/bin/busybox ash
+
+. /etc/init.d/tc-functions
+. /var/www/cgi-bin/pcp-functions
+
+useBusybox
+TARGET=`cat /etc/sysconfig/backup_device`
+cd /mnt/$TARGET
+echo naa.tcz >>onboot.lst
+cd optional
+wget https://raw.githubusercontent.com/sam0402/pCP-addon/HPPlayer/naa.tcz
+
+pcp_write_var_to_config USER_COMMAND_3 ""
+echo "Rebooting..."
+sleep 3
+pcp br
