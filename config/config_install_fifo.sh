4b30012f79ceb4bd25204c6a0bf7a52c833378c0
sam0402 <sam0402@gmail.com>
2021-10-14 13:13:49 +0800

    Update config_install_fifo.sh

--- config/config_install_fifo.sh
+++ config/config_install_fifo.sh
@@ -15,7 +15,7 @@ cd optional
 sed -i '/alsa-utils.tcz/d;/rng-tools.tcz/d;/dialog.tcz/d' pcp.tcz.dep
 sed -i '/glib2.tcz/d' libudev.tcz.dep
 echo 'libasound2.tcz' >pcp-squeezelite.tcz.dep
-pcp_write_var_to_config USER_COMMAND_1 "uhubctl+-l2+-a0%3Bsleep+180%3Bpkill+-f+httpd"
+pcp_write_var_to_config USER_COMMAND_1 "uhubctl+-l2+-a0%3Bsleep+180%3Bpkill+-f+httpd%3Bumount+%2Ftmp%2Ftcloop%2Fpcp-8.0.0-www"
 pcp_write_var_to_config USER_COMMAND_2 "sleep+180%3b%5b+%60pgrep+ssh+%7c+wc+-l%60+-lt+3+%5d+%26%26+pkill+-f+ssh"
 
 sed -i '11,$d' /opt/bootlocal.sh
