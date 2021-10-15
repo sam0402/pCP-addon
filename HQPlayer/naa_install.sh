26fedc9f819d54166997ea3eb2f22a36be231999
sam0402 <sam0402@gmail.com>
2021-10-16 07:25:29 +0800

    Update naa_install.sh

--- HQPlayer/naa_install.sh
+++ HQPlayer/naa_install.sh
@@ -6,11 +6,11 @@
 useBusybox
 TARGET=`cat /etc/sysconfig/backup_device`
 cd /mnt/$TARGET
-sed -i '/naa.tcz/d' onboot.lst
-echo naa.tcz >>onboot.lst
+sed -i '/naa.tcz/d;/naa_arm64.tcz/d' onboot.lst
+echo naa_arm64.tcz >>onboot.lst
 cd optional
 tce-load -i ca-certificates.tcz
-wget https://raw.githubusercontent.com/sam0402/pCP-addon/main/HQPlayer/naa.tcz
+wget https://raw.githubusercontent.com/sam0402/pCP-addon/main/HQPlayer/naa_arm64.tcz
 
 pcp_write_var_to_config USER_COMMAND_3 "(networkaudiod+%26)%3b+taskset+-p+0x00000008+%24(pgrep+networkaudiod*)"
 echo "Rebooting..."
