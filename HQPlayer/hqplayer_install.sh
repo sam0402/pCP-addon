9c0ba5ca019739d76b0ef7befeb1de5227c70d91
sam0402 <sam0402@gmail.com>
2021-10-19 09:29:43 +0800

    Create hqplayer_install.sh

---
+++ HQPlayer/hqplayer_install.sh
@@ -0,0 +1,27 @@
+
+#!/bin/busybox ash
+
+. /etc/init.d/tc-functions
+. /var/www/cgi-bin/pcp-functions
+
+useBusybox
+TARGET=`cat /etc/sysconfig/backup_device`
+cd /mnt/$TARGET
+sed -i '/hqplayer.tcz/d' onboot.lst
+echo hqplayer.tcz >>onboot.lst
+cd optional
+tce-load -i ca-certificates.tcz
+wget https://raw.githubusercontent.com/sam0402/pCP-addon/main/HQPlayer/hqplayer.tcz
+echo 'http://repo.tinycorelinux.net/' >/opt/tcemirror
+sed -i '/ldconfig/d;/hqplayerd/d' /opt/bootlocal.sh
+echo 'ldconfig' >>/opt/bootlocal.sh
+echo '(sudo -u tc hqplayerd&);taskset -p 0x00000008 $(pgrep hqplayerd*)' >>/opt/bootlocal.sh
+mkdir -p ~/hqplayer/modules-load.d
+mkdir -p ~/hqplayer/udev/rules.d
+cd ~/hqplayer
+wget https://raw.githubusercontent.com/sam0402/pCP-addon/main/HQPlayer/hqplayerd.xml
+wget https://raw.githubusercontent.com/sam0402/pCP-addon/main/HQPlayer/hqplayerd-auth.xml
+pcp_write_var_to_config USER_COMMAND_3 ""
+echo "Rebooting..."
+sleep 3
+pcp br
