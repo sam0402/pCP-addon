ce566cc148ba9f57c35ce343cc5cd49a08e6ffec
sam0402 <sam0402@gmail.com>
2021-10-19 12:11:41 +0800

    Update config_usb_dac.sh

--- config/config_usb_dac.sh
+++ config/config_usb_dac.sh
@@ -17,6 +17,7 @@ sed -i '/glib2.tcz/d' libudev.tcz.dep
 echo 'libasound2.tcz' >pcp-squeezelite.tcz.dep
 pcp_write_var_to_config USER_COMMAND_1 "uhubctl+-l2+-a0%3Buhubctl+-l2+-p2+-a1%3Bsleep+120%3Bpkill+-f+httpd%3Bumount+%2Ftmp%2Ftcloop%2Fpcp-8.0.0-www"
 pcp_write_var_to_config USER_COMMAND_2 "sleep+180%3b%5b+%60pgrep+ssh+%7c+wc+-l%60+-lt+3+%5d+%26%26+pkill+-f+ssh"
+pcp_write_var_to_config USER_COMMAND_3 ""
 
 sed -i '11,$d' /opt/bootlocal.sh
 if [ `grep -c 'taskset' /opt/bootlocal.sh` -eq 0 ]
