bb42a6e5c0a718e50b13a2e990b71bab18bd3dcb
sam0402 <sam0402@gmail.com>
2021-10-19 23:08:26 +0800

    Update optimal_sound.sh

--- config/optimal_sound.sh
+++ config/optimal_sound.sh
@@ -12,7 +12,7 @@ cd optional
 sed -i '/rng-tools.tcz/d;/dialog.tcz/d' pcp.tcz.dep
 sed -i '/glib2.tcz/d' libudev.tcz.dep
 echo 'libasound2.tcz' >pcp-squeezelite.tcz.dep
-pcp_write_var_to_config USER_COMMAND_1 "%23sleep+60%3buhubctl+-l2+-a0%3bsleep+120%3bpkill+-f+httpd"
+pcp_write_var_to_config USER_COMMAND_1 "%23sleep+60%3buhubctl+-l2+-a0%3bsleep+180%3bpkill+-f+httpd"
 pcp_write_var_to_config USER_COMMAND_2 "sleep+180%3bif+%5b+%60pgrep+ssh+%7c+wc+-l%60+-lt+3+%5d%3bthen+pkill+-f+ssh%3bfi"
 
 sed -i '11,$d' /opt/bootlocal.sh
